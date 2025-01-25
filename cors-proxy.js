40005f1a50f54a8afecc193e9551d91494667bb0
someoneacccount238 <someoneacccount238@gmail.com>
2025-01-25 19:19:10 +0200

    f s

--- cors-proxy.js
+++ cors-proxy.js
@@ -34,11 +34,20 @@ const proxy = httpProxy.createProxyServer({
     timeout: 10000, // Set timeout (in milliseconds)
 });
 
+// Add CORS headers to proxy responses (place it here)
+proxy.on('proxyRes', (proxyRes, req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+});
+
+
 // Middleware to handle CORS headers
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
     if (req.method === 'OPTIONS') {
         return res.sendStatus(200);
     }
